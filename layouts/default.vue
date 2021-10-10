04cabcf210dfb1ec00a812b7ab2c400230be97e6
Jakub <jacob98k@hotmail.com>
2021-10-11 01:24:32 +0200

    poprawka wyswietlania search inputa

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
             <Hero />
         </template>
         <div class="container mx-auto px-3 lg:px-0">
-            <template v-if="!$nuxt.$route.name == 'auth-register'">
+            <template v-if="$nuxt.$route.name != 'auth-register'">
                 <SearchInput />
             </template>
             <Nuxt />
